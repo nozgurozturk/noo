ef6f2d95cb4118debefbeaf030090149c9f46317
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-09-30 01:38:47 +0300

    feat: add github graphql api provider

---
+++ api/github/base.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const got = require('got')
+/**
+ * @namespace Github
+ * @description Github GraphQL API Provider
+ */
+class Github {
+  constructor() {
+    this.baseUrl = 'https://api.github.com/graphql'
+    this.token = process.env.GITHUB_AUTH_KEY
+  }
+
+  /**
+   * @method query
+   * @param {string} query
+   * @description GraphQL Query
+   */
+  async query({query}) {
+    console.log('token',this.token)
+    console.info(query)
+    try {
+      const { body } = await got.post(this.baseUrl, {
+        headers: {
+          Authorization: `bearer ${this.token}`
+        },
+        body: JSON.stringify({query}),
+        responseType : 'json'
+      })
+      if (!body) {
+        throw new Error('Unhandled error')
+      }
+      if (body.errors) {
+        throw new Error(JSON.stringify(body.errors))
+      }
+      console.log('body',body)
+      const data = body.data
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+module.exports = Github
